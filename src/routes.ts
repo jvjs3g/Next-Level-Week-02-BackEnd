1de70acf4079a6c5cbcf9d3d8a87df1615a2e9ae
joao vitor <joao3klb@hotmail.com>
2020-08-07 21:39:37 -0300

    BackEnd Finalizado

--- src/routes.ts
+++ src/routes.ts
@@ -1,70 +1,17 @@
 import express from 'express';
-import db from './database/connection';
+import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
-import converteHoursToMinutes from './utils/convertHoursToMinutes';
 
 const routes = express.Router();
 
-interface scheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
+const classesController = new ClassesController();
+const connectionsController = new  ConnectionController();
 
-routes.post('/classes', async  (request,response) => {
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
-
-  const trx = await db.transaction();
-
-  try{  
-    const insertdUsersIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio
-    });
-  
-    const user_id = insertdUsersIds[0];
-  
-    const insertedClassesId = await trx('classes').insert({
-      subject,
-      cost,
-      user_id
-    });
-  
-    const class_id = insertedClassesId[0]; 
-    
-    const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: converteHoursToMinutes(scheduleItem.from),
-        to: converteHoursToMinutes(scheduleItem.to),
-      };
-    })
-  
-    await trx('class_schedule').insert(classSchedule);
-  
-    await trx.commit();
-  
-    return response.status(201).send();
-
-  }catch(err){
-    await trx.rollback();
-
-    return response.status(400).json({
-      error: 'Unexpected error while creating new class',
-    });
-  }
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
